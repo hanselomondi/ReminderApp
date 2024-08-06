d262ddffcf7fb5ed34d00b85591a2247b409933d
Hansel Omondi <ogopabhs@gmail.com>
2024-08-06 15:17:46 +0300

    Add: domain model for a Reminder
    This data class will be used to create a Reminder instance

---
+++ app/src/main/java/com/example/remindme/reminder/domain/model/Reminder.kt
@@ -0,0 +1,13 @@
+package com.example.remindme.reminder.domain.model
+
+data class Reminder(
+    val title: String,
+    val description: String? = null,
+    val dueDate: Long,
+    val dueTime: Long? = null,
+    val priority: ReminderPriority,
+    val tags: List<String> = emptyList(),
+    val createdAt: Long = System.currentTimeMillis(),
+    val updatedAt: Long = System.currentTimeMillis(),
+    val isCompleted: Boolean = false
+)
