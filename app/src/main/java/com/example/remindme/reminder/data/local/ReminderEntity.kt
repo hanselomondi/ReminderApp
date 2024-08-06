1147ddfb704c46c8b386d1a14b799b6185757592
Hansel Omondi <ogopabhs@gmail.com>
2024-08-06 15:48:55 +0300

    Add: ReminderEntity to represent a db table
    This data class is a Room Entity and represents a database table

---
+++ app/src/main/java/com/example/remindme/reminder/data/local/ReminderEntity.kt
@@ -0,0 +1,21 @@
+package com.example.remindme.reminder.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.remindme.reminder.domain.model.ReminderPriority
+
+@Entity(tableName = "reminder_table")
+data class ReminderEntity(
+    val title: String = "",
+    val description: String? = null,
+    val dueDate: Long = 0L,
+    val dueTime: Long? = null,
+    val priority: ReminderPriority,
+    val tags: List<String> = emptyList(),
+    val createdAt: Long = 0L,
+    val updatedAt: Long = 0L,
+    val isCompleted: Boolean
+) {
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+}
