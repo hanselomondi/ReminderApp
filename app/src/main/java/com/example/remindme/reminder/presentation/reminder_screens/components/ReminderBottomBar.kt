81363cd20c68c99c3b3b95f524c62cdee4ce7439
Hansel Omondi <ogopabhs@gmail.com>
2024-08-14 10:14:12 +0300

    Update: appearnce of ReminderBottomBar
    Previously, this component used the BottomAppBar Composable and a Row
    that used the `BottomRowItem`. This has been changed to NavigationBar
    Composable as well as defining a data class for a BottomNavigationItem

--- app/src/main/java/com/example/remindme/reminder/presentation/reminder_screens/components/ReminderBottomBar.kt
+++ app/src/main/java/com/example/remindme/reminder/presentation/reminder_screens/components/ReminderBottomBar.kt
@@ -1,33 +1,32 @@
 package com.example.remindme.reminder.presentation.reminder_screens.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.outlined.CheckCircleOutline
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.remindme.R
 import com.example.remindme.ui.theme.RemindMeTheme
 import com.example.remindme.util.ScreenNames
+
+data class BottomNavigationItem(
+    val destination: ScreenNames,
+    val filledIcon: ImageVector,
+    val outlinedIcon: ImageVector,
+    val onClick: () -> Unit,
+    val label: String
+)
+
 
 @Composable
 fun ReminderBottomBar(
@@ -36,47 +35,39 @@ fun ReminderBottomBar(
     onHomeClicked: () -> Unit,
     onCompletedClicked: () -> Unit
 ) {
-    BottomAppBar(modifier = modifier) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            BottomRowItem(
-                icon = if (currentScreen == ScreenNames.HomeScreen) Icons.Filled.Home else Icons.Outlined.Home,
-                label = stringResource(R.string.home),
-                onIconClicked = { onHomeClicked() }
-            )
-            BottomRowItem(
-                icon = if (currentScreen == ScreenNames.CompletedRemindersScreen) Icons.Filled.CheckCircle else Icons.Outlined.CheckCircleOutline,
-                label = stringResource(R.string.completed),
-                onIconClicked = { onCompletedClicked() }
-            )
-        }
-    }
-}
+    val items = listOf(
+        BottomNavigationItem(
+            destination = ScreenNames.HomeScreen,
+            filledIcon = Icons.Filled.Home,
+            outlinedIcon = Icons.Outlined.Home,
+            onClick = onHomeClicked,
+            label = stringResource(R.string.home)
+        ),
+        BottomNavigationItem(
+            destination = ScreenNames.CompletedRemindersScreen,
+            filledIcon = Icons.Filled.CheckCircle,
+            outlinedIcon = Icons.Outlined.CheckCircleOutline,
+            onClick = onCompletedClicked,
+            label = stringResource(R.string.completed)
+        )
+    )
 
-
-@Composable
-fun BottomRowItem(
-    modifier: Modifier = Modifier,
-    icon: ImageVector,
-    label: String,
-    onIconClicked: () -> Unit
-) {
-    BadgedBox(badge = {}, modifier = modifier) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            IconButton(
-                onClick = onIconClicked
-            ) {
-                Icon(
-                    imageVector = icon,
-                    contentDescription = label
-                )
-            }
-            Spacer(modifier = Modifier.height(2.dp))
-            Text(
-                text = label,
-                style = MaterialTheme.typography.bodySmall
+    NavigationBar(modifier = modifier) {
+        items.forEach { item ->
+            NavigationBarItem(
+                selected = currentScreen == item.destination,
+                onClick = item.onClick,
+                icon = {
+                    BadgedBox(badge = {}) {
+                        Icon(
+                            imageVector = if (currentScreen == item.destination) item.filledIcon else item.outlinedIcon,
+                            contentDescription = item.destination.title
+                        )
+                    }
+                },
+                label = {
+                    Text(text = item.label)
+                }
             )
         }
     }
@@ -104,32 +95,6 @@ private fun BottomBarLightPreview() {
             currentScreen = ScreenNames.HomeScreen,
             onHomeClicked = {},
             onCompletedClicked = {}
-        )
-    }
-}
-
-
-@Preview(name = "BottomRowItemDark", showBackground = true)
-@Composable
-private fun BottomRowItemDarkPreview() {
-    RemindMeTheme(darkTheme =  true) {
-        BottomRowItem(
-            icon = Icons.Filled.Home,
-            label = "Home",
-            onIconClicked = {}
-        )
-    }
-}
-
-
-@Preview(name = "BottomRowItemLight", showBackground = true)
-@Composable
-private fun BottomRowItemLightPreview() {
-    RemindMeTheme(darkTheme =  true) {
-        BottomRowItem(
-            icon = Icons.Filled.Home,
-            label = "Home",
-            onIconClicked = {}
         )
     }
 }
